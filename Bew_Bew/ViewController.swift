21dbd33d5c678d1bdfdf906145d056d5cc3b4521
Jordan Riser <riser.jordan@gmail.com>
2015-08-04 13:45:19 -0400

    Initial Commit

---
+++ Bew_Bew/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bew_Bew
+//
+//  Created by Jordan Riser on 8/4/15.
+//  Copyright (c) 2015 Jordan Riser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
